e54d5d5804f5e7e07e22ed5207839ad26b4bfd73
Tim Godfrey <timg@commoncode.com.au>
2016-11-04 00:55:44 +1100

    Implemented image uploader

--- djropbox_uploader.py
+++ djropbox_uploader.py
@@ -1,4 +1,14 @@
+#!/usr/bin/env python3
 import os
+import requests
+import json
+
+
+def normalise_endpoint(endpoint):
+    """
+    Nice little method to ensure the right amount of slashes are in every endpoint string
+    """
+    return '/{}/'.format('/'.join([crumb for crumb in endpoint.split('/') if crumb]))
 
 
 class DjropboxUploader:
@@ -10,8 +20,53 @@ class DjropboxUploader:
         self.host = host if host else os.environ.get('DJROPBOX_HOST')
         self.username = username if username else os.environ.get('DJROPBOX_USERNAME')
         self.password = password if password else os.environ.get('DJROPBOX_PASSWORD')
+        self.auth_token = None
+
+    def gen_headers(self):
+        headers = {}
+        if self.auth_token:
+            headers['Authorization'] = 'Token {}'.format(self.auth_token)
+        return headers
+
+    def post(self, endpoint, data={}, is_json=True, **kwargs):
+        """
+        Post the data to the endpoint
+        """
+        url = 'http://{}{}'.format(self.host, normalise_endpoint(endpoint))
+        headers = self.gen_headers()
+
+        if is_json:
+            headers['Content-Type'] = 'application/json'
+            post_data = json.dumps(data)
+        else:
+            post_data = data
+
+        return requests.post(url, data=post_data, headers=headers, **kwargs)
+
+    def authenticate(self):
+        response = self.post('/obtain-auth-token/', data={
+            'username': self.username,
+            'password': self.password,
+        })
+        assert response.status_code == requests.codes.ok
+        self.auth_token = response.json()['token']
 
     def upload(self, file_path):
         """Upload the file"""
+        if not self.auth_token:
+            # No auth token. Attempt to authenticate
+            self.authenticate()
+
         print('Uploading {}'.format(file_path))
-        pass
+
+        with open(file_path, 'rb') as file_data:
+            response = self.post(
+                '/api/images/', is_json=False,
+                files={'image': (os.path.basename(file_path), file_data)}
+            )
+            response.raise_for_status()
+
+
+if __name__ == '__main__':
+    uploader = DjropboxUploader()
+    uploader.upload('/some/file/path')
